38add9c5bb3094c3aff8a7202bed0deb95177f0a
tai chen <tai@brixtondynamics.co.uk>
2018-11-22 17:39:18 +0100

    Initial Commit

---
+++ BabyDoodlePad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BabyDoodlePad
+//
+//  Created by tai chen on 22/11/2018.
+//  Copyright © 2018 TPBSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
